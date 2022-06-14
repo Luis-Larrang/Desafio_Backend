ae425ffb2bf53153362b7e1b8f6fb513785b2d5f
Luis Larrang <luislarrang@gmail.com>
2022-06-14 19:29:07 -0300

    desafio 1 consulta

--- funciones.jsx
+++ funciones.jsx
@@ -1,28 +1,36 @@
-class usuario {
+//CUANDO INTETNO USAR CLASS ME DICE QUE NO ES FUNCION O NO RECONOCE LOS PARAMETROS.
+/*class usuario {
     constructor(nombre,apellido,libros,mascotas){
         nombre = "Antonela";
         apellido = "Fernandez";
         libros = {};
         mascotas = [];        
     }
-// getFullName(){
-//     console.log(nombre + " " + apellido)
-// }
-// addMascota(name){
-//     mascotas.push(name)
-// }
-// addBook(nombre,autor){
-//     libros.push([nombre,autor])
-// }
-// countMascotas(){
-//     return console.log(mascotas)
-// }
-// getBookNames(){
-//     return console.log(libros)
-// }    
-}
+ getFullName(){
+     console.log(nombre + " " + apellido)
+ }
+ addMascota(name){
+     mascotas.push(name)
+ }
+ addBook(nombre,autor){
+     libros.push([nombre,autor])
+ }
+ countMascotas(){
+     return console.log(mascotas.length)
+ }
+ getBookNames(){
+     return console.log(libros)
+ }    
+}*/
+
+//FUNCIONA PERO SIN CLASS
+const nombre = "Antonela";
+const apellido = "Fernandez";
+const libros = [];
+const mascotas = []; 
+
 function getFullName(){
-    console.log(usuario.nombre + " " + usuario.apellido)
+    console.log(nombre + " " + apellido)
 }
 function addMascota(name){
     mascotas.push(name)
@@ -31,14 +39,15 @@ function addBook(nombre,autor){
     libros.push([nombre,autor])
 }
 function countMascotas(){
-    return console.log(mascotas)
+    return console.log(mascotas.length)
 }
 function getBookNames(){
     return console.log(libros)
 } 
 
 getFullName();
-addMascota("Tiranosautio");
+addMascota("Tiranosaurio");
+addMascota("Lechuza");
 addBook("El señor de los anillos", "Tolkien");
 countMascotas();
 getBookNames();
