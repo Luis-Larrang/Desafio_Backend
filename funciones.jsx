6a12e11450a0e3a5644e23405861dfb4bd533d81
Luis Larrang <luislarrang@gmail.com>
2022-06-14 21:55:09 -0300

    entrega 1 consulta 4

--- funciones.jsx
+++ funciones.jsx
@@ -4,67 +4,37 @@ class usuario {
         this.nombre = nombre;
         this.apellido = apellido;
         //Tengo duda si en libros va como array [] o directo objeto {} para hacer el push.
-        this.libros = {};
+        this.libros = [];
         this.mascotas = [];        
-    } 
- addMascota(name){
-     this.mascotas.push(name);
- }
+    }
+    getFullName(){
+        console.log(this.nombre + " " + this.apellido);
+    }        
+    addMascota(name){
+        this.mascotas.push(name);
+    }
  //De nuevo aca dependiendo de la respuesta a la duda anterior no se si deberia ponerle los {}.
- addBook(nombre,autor){
-     this.libros.push(nombre,autor);
- }
+    addBook(nombre,autor){
+        this.libros.push({nombre,autor});
+    }
+    countMascotas(){
+    return console.log(this.mascotas.length)
+    }
+    //Me falta nomas hacer que solamente me diga el nombre del libro en lugar de todo el array
+    getBookNames(){
+        return console.log(this.libros)
+    } 
 }
 
 //Creacion de nuevo usuario pero sin mascotas ni libros
 
 let usuario1 = new usuario("Antonela", "Fernandez");
 
-//Llamado a nombre completo funciona
-
-function getFullName(e){
-    console.log(e.nombre + " " + e.apellido);
-}
-
-//No puedo probar si funcionan porque tengo errores previos.
-function countMascotas(e){
-    return console.log(e.mascotas.length)
-}
-function getBookNames(e){
-    return console.log(e.libros)
-} 
-
-/*FUNCIONA PERO SIN CLASS
-
-const nombre = "Antonela";
-const apellido = "Fernandez";
-const libros = {};
-const mascotas = []; 
-
-function getFullName(){
-    console.log(nombre + " " + apellido)
-}
-function addMascota(name){
-    mascotas.push(name)
-}
-function addBook(nombre,autor){
-    libros.push(nombre,autor)
-}
-function countMascotas(){
-    return console.log(mascotas.length)
-}
-function getBookNames(){
-    return console.log(libros)
-} 
-*/
-
-//Llamado funciona.
-getFullName(usuario1);
-
-//Me dice que addMascota no esta definido, intente poner usuario.addMascota pero tampoco. Lo mismo pasa con addBook()
-addMascota("Tiranosaurio");
-addBook("El señor de los anillos", "Tolkien");
-//Aca me surge la duda de si debo poner usuario u otra cosa.
-countMascotas(usuario);
-getBookNames(usuario);
+usuario1.getFullName();
+usuario1.addMascota("Tiranosaurio");
+usuario1.addMascota("Perro");
+usuario1.addBook("El señor de los anillos", "Tolkien");
+usuario1.addBook("Alexandros", "Massimo Manfredi");
+usuario1.countMascotas();
+usuario1.getBookNames();
 
